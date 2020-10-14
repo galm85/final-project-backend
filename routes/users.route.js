e7fc7353ce83ce045a0fccb88c735ffef232b01e
galm85 <galm85@gmail.com>
2020-10-14 21:15:39 +0300

    add to user route add&remove&get favorite

--- routes/users.route.js
+++ routes/users.route.js
@@ -2,6 +2,9 @@ const router = require('express').Router();
 const bcrypt = require('bcrypt');
 const {User,validateUser,validateFav} = require('../models/user.model');
 
+
+
+
 //register a new user account
 router.post("/", async (req,res)=>{
  const {error} = validateUser(req.body);
@@ -24,35 +27,61 @@ router.post("/", async (req,res)=>{
 router.post('/sign-in',async(req,res)=>{
     
     let user = await User.findOne({email:req.body.email});
-    
     if (!user) {
     return  res.status(400).send('Email or Password is incorrect');
     }
 
     let compare = await bcrypt.compare(req.body.password, user.password);
-    
     if(!compare){
         return  res.send('Email or Password is incorrect');
     }
 
     return res.json({token:user.generateAuthToken()});
-
 })
 
-//update User
+
+//edit User
 router.put("/update-user", async (req,res)=>{
-    // const {error} = validateUser(req.body);
-    // if (error)  return res.send(error.details[0].message);
+    const {error} = validateUser(req.body);
+    if (error)  return res.send(error.details[0].message);
    
     let user = await User.findOneAndUpdate({_id:req.body._id},req.body);
     if (!user) {
        return  res.status(400).send('No user');
-    
     }
+
     res.status(200).json({token:user.generateAuthToken()});
     
    })
 
+
+
+
+//get all favorites review of a user
+router.get('/favorite/:userId',async (req,res)=>{
+    try{
+     let favs =  await User.findOne({_id:req.params.userId},{fav:1});
+     res.status(200).send(favs);
+
+        
+    }catch(error){
+        res.status(400).send(error);
+    }
+})
+
+
+//add to review to the user's favorite list
+router.put('/favorite/:userId',async (req,res)=>{
+    try {
+       let user = await User.findOneAndUpdate({_id:req.params.userId},{$push:{fav:req.body}});
+       res.status(200).send("user");
+        
+    }
+    catch(error){
+        console.log(error);
+        res.status(400).send('not ok')
+    }
+})
 
 
    //remove from favor list
